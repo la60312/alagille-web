369a9d49ce72abdc7a12a0703a1fddf03e0a71c5
Tzung-Chien Hsieh <la60312@gmail.com>
2024-10-17 09:51:19 +0200

    Update App.js
    Add webstie title

--- client/src/App.js
+++ client/src/App.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import './App.css';
 import AboutUs from './AboutUs';
 import ProjectDescription from './ProjectDescription';
@@ -6,7 +6,11 @@ import References from './References';
 import Footer from './Footer';
 
 function App() {
-  return (
+    useEffect(() => {
+        document.title = 'GestaltMatcher for Alagille Syndrome';
+    }, []);
+
+    return (
       <div className="App">
           <header className="App-header">
               <h1>Screening for Alagille Syndrome by facial image analysis with artificial intelligence</h1>
@@ -21,7 +25,7 @@ function App() {
 
 
       </div>
-  );
+    );
 }
 
 export default App;
